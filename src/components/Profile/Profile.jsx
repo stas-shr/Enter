adb47d7c27810c9aa7e72354f5551d837ad3c48f
stas-shr <stasshr@gmail.com>
2022-02-08 16:49:39 +0300

    Added new 'Dialog' page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={classes.profile}>
+        <div>
             <div>
                 <img src="https://www.fao.org/uploads/pics/Corals_03.jpg" />
             </div>
